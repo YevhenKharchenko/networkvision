f966e90119cbfe3fa248df3c838372d194661d96
YevhenKharchenko <evgenvalerych@gmail.com>
2026-03-16 20:54:23 +0200

    Initial commit

---
+++ src/js/gallery.js
@@ -0,0 +1,52 @@
+import Swiper from 'swiper';
+import 'swiper/css/bundle';
+
+const galleryLeftArrow = document.getElementById('galleryLeftArrow');
+const galleryRightArrow = document.getElementById('galleryRightArrow');
+
+let gallerySwiper;
+
+gallerySwiper = new Swiper('.gallery-swiper-container', {
+  direction: 'horizontal',
+  loop: false,
+  centeredSlides: true,
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  initialSlide: 0,
+  spaceBetween: 16,
+  speed: 500,
+  allowTouchMove: true,
+  grabCursor: true,
+  breakpoints: {
+    1440: {
+      slidesPerView: 'auto',
+      initialSlide: 2,
+      spaceBetween: 24,
+    },
+  },
+
+  on: {
+    init(swiper) {
+      document.querySelector('.gallery-swiper-container').classList.add('show');
+      updateGalleryArrows(swiper);
+    },
+    slideChange(swiper) {
+      updateGalleryArrows(swiper);
+    },
+  },
+});
+
+updateGalleryArrows(gallerySwiper);
+
+function updateGalleryArrows(swiper) {
+  galleryLeftArrow.disabled = swiper.isBeginning;
+  galleryRightArrow.disabled = swiper.isEnd;
+}
+
+galleryLeftArrow.addEventListener('click', () => {
+  gallerySwiper.slidePrev();
+});
+
+galleryRightArrow.addEventListener('click', () => {
+  gallerySwiper.slideNext();
+});
